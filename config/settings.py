67a087ccaaa0fe6e3e0dfbee933674c0a9d8cd83
Sayidboy7 <sayidboyyoldoshov@gmail.com>
2026-07-24 13:08:31 +0500

    modifiying

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,9 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_MEDIA = '/media/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = (
